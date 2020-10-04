c745dc6f17053ab19a264f3b3f8ef454eb7838cb
German Gonzalez <german@vetta.io>
2020-10-03 20:38:52 -0500

    main - se trabajo con el header, se agregó la imagen de fondo y el navbar.

--- maquetacion-tenis/src/App.vue
+++ maquetacion-tenis/src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <div id="app">
-    <p>hola</p>
+    <header>
+      <Navbar class="navbar-component"></Navbar>
+    </header>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+#app{
+  font-family: 'HelveticaNeue';
+  header{
+    display: flex;
+    background: url('assets/images/group@2x.png');
+    background-size: cover;
+    background-position: right;
+    background-repeat: no-repeat;
+    height: 785px;
+  }
+  .navbar-component{
+    width: 100%;
+  }
+}
 </style>
---
+++ maquetacion-tenis/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tennis-nav">
+    <b-navbar toggleable="lg" type="dark">
+    <b-navbar-brand href="#">
+        <img src="@/assets/images/tennisBall.svg" alt="tennis-ball">
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Inicio</b-nav-item>
+        <b-nav-item href="#">Acerca de nosotros</b-nav-item>
+        <b-nav-item href="#">Historia</b-nav-item>
+        <b-nav-item href="#">Contacto</b-nav-item>
+      </b-navbar-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">ES</b-nav-item>
+        <b-nav-item href="#">EN</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
